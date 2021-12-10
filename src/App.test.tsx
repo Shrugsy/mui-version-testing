2205415fe4cb68689b8701d24989b0daa9a02ba0
Josh Fraser <joshfraser91@gmail.com>
2021-12-10 11:20:56 +1100

    🎉 Add initial code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const numIterations = 100;
+
+for (let i = 0; i < numIterations; i++) {
+  it(`${i} - can fill out a form`, () => {
+    render(<App />);
+    const nameInput = screen.getByRole("textbox", {
+      name: /name/i,
+    });
+    const ageInput = screen.getByRole("spinbutton", {
+      name: /age/i,
+    });
+    const titleInput = screen.getByRole("textbox", {
+      name: /title/i,
+    });
+
+    userEvent.type(nameInput, "john");
+    userEvent.type(ageInput, "23");
+    userEvent.type(titleInput, "carpenter");
+    userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+    screen.getByText(/Submitted data: Name: john, Age: 23, Title: carpenter/i);
+  });
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import { Button, TextField } from "@mui/material";
+import { useCallback, useState, ChangeEvent } from "react";
 
-function App() {
+const useFormField = (initialValue: string, label: string) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) =>
+      setValue(e.target.value),
+    []
+  );
+  const clearValue = useCallback(() => setValue(""), []);
+  return [
+    { value, onChange, label },
+    { clearValue, setValue }
+  ] as const;
+};
+
+const Form = styled("form")`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  margin: 0 auto;
+  gap: 8px;
+`;
+
+export default function App() {
+  const [nameField, nameHandlers] = useFormField("", "Name");
+  const [ageField, ageHandlers] = useFormField("", "Age");
+  const [titleField, titleHandlers] = useFormField("", "Title");
+  const [submittedData, setSubmittedData] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSubmittedData(
+            `Name: ${nameField.value}, Age: ${ageField.value}, Title: ${titleField.value}`
+          );
+
+          nameHandlers.clearValue();
+          ageHandlers.clearValue();
+          titleHandlers.clearValue();
+        }}
+      >
+        <TextField {...nameField} />
+        <TextField {...ageField} type="number" />
+        <TextField {...titleField} />
+        <Button type="submit" variant="outlined">
+          Submit
+        </Button>
+      </Form>
+      <div>Submitted data: {submittedData || "None"}</div>
     </div>
   );
 }
-
-export default App;
